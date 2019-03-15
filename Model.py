941de2332fac65203a34c74ac8df84739147d84a
Maryam Ehab <maryammehab@gmail.com>
2019-03-15 16:53:36 +0200

    model

--- Model.py
+++ Model.py
@@ -5,24 +5,44 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from decimal import Decimal, getcontext
+from pathlib import Path
 
 Base = declarative_base()
-
-
-class Song(Base):
-    __tablename__ = 'complain'
-    # Here we define columns for the table Song
-    # Notice that each column is also a normal Python instance attribute.
-    ID = Column(Integer, primary_key=True)
-    branch_ID = Column(Integer, ForeignKey('customer.id'))
-    restaurant_ID = Column(Integer, ForeignKey('customer.id'))
-    customer_ID = Column(Integer, ForeignKey('customer.id'))
-    # relations
 
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
     ID = Column(Integer, primary_key=True)
-    name = Column(String)
-    branchs = Column()
+    name = Column(String(50))
+    '''
+    branchs = Column([])
+
+    locations = Column()
+    openTime = Column()
+    closeTime = Column()'''
+
+
+class Complain(Base):
+    __tablename__ = 'complain'
+    ID = Column(Integer, primary_key=True)
+    name = Column(Integer)
+    # branch_ID = Column(Integer, ForeignKey('branch.id'))
+    restaurant_ID = Column(Integer, ForeignKey('restaurant.ID'))
+    # customer_ID = Column(Integer, ForeignKey('customer.id'))
+
+
+engine = create_engine('sqlite:///p.db')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+def addToDB():
+    complain = Complain(name=1)
+    session.add(complain)
+    session.commit()
 
