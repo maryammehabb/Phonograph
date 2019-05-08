69be57b21776758722600a6989b600c722f36f01
Marwa Belal <marwabelal105@gmail.com>
2019-05-08 16:47:12 +0200

    list

--- dialogflow-python-client-master/examples/slotfilling_example/try.py
+++ dialogflow-python-client-master/examples/slotfilling_example/try.py
@@ -1,55 +1,59 @@
 import  apiai
 import json
-def main():
-    ai = apiai.ApiAI('cd4fbf5bf0bb489784266935eb751421')
-    while True:
-        print(u"> ", end=u"")
-        user_message = input()
 
-        if user_message == u"exit":
-            break
+ai = apiai.ApiAI('cd4fbf5bf0bb489784266935eb751421')
+while True:
+    print(u"> ", end=u"")
+    user_message = input()
+    if user_message == u"exit":
+        break
 
-        request = ai.text_request()
-        request.query = user_message
+    request = ai.text_request()
+    request.query = user_message
 
-        response = json.loads(request.getresponse().read())
+    response = json.loads(request.getresponse().read())
 
-        result = response['result']
-        action = result['action']
-        actionIncomplete = result.get('actionIncomplete', False)
+    result = response['result']
+    action = result['action']
+    actionIncomplete = result.get('actionIncomplete', False)
 
-        print(u"< %s" % response['result']['fulfillment']['speech'])
+    print(u"< %s" % response['result']['fulfillment']['speech'])
 
-        if action is not None:
-            if action == u"send_message":
-                parameters = result['parameters']
+    if action is not None:
+        if action == u"send_message":
+            parameters = result['parameters']
 
-                text = parameters.get('text')
-                message_type = parameters.get('message_type')
-                parent = parameters.get('parent')
+            text = parameters.get('text')
+            message_type = parameters.get('message_type')
+            parent = parameters.get('parent')
 
-                print (
-                    'text: %s, message_type: %s, parent: %s' %
-                    (
-                        text if text else "null",
-                        message_type if message_type else "null",
-                        parent if parent else "null"
-                    )
+            print (
+                'text: %s, message_type: %s, parent: %s' %
+                (
+                    text if text else "null",
+                    message_type if message_type else "null",
+                    parent if parent else "null"
                 )
+            )
 
-                if not actionIncomplete:
-                    print(u"...Sending Message...")
-                    break
-            #print("Class Name: "+response['result']['metadata']['intentName'])
-            if (response['result']['metadata']['intentName'] == 'make reservation'):
-                number = response['result']['parameters']['number']
-                date = response['result']['parameters']['date']
-                time = response['result']['parameters']['time']
-                if(number != '' and date !='' and time !=''):
-                    print("Class Name: "+response['result']['metadata']['intentName'])
-                    print("Number: "+response['result']['parameters']['number'])
-                    print("Date: "+response['result']['parameters']['date'])
-                    print("Time: "+response['result']['parameters']['time'])
+            if not actionIncomplete:
+                print(u"...Sending Message...")
+                break
 
-main()
 
+        #print("Class Name: "+response['result']['metadata']['intentName'])
+        # class make reservation
+        if (response['result']['metadata']['intentName'] == 'make reservation'):
+            number = [response['result']['parameters']['number']]
+            date = [response['result']['parameters']['date']]
+            time = [response['result']['parameters']['time']]
+            if(len(number)!=1 and len(date) != 1 and len(time) != 1):
+                for x in range(len(number)):
+                    print(number[x])
+                for x in range(len(date)):
+                    print(date[x])
+                for x in range(len(time)):
+                    print(time[x])
+        # class edit reservation
+        # class delete reservation
+        
