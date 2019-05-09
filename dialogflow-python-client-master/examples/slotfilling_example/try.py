f85a00d9d102c12133d6cabc2abf1cce9af14c2b
Marwa Belal <marwabelal105@gmail.com>
2019-05-09 13:27:42 +0200

    Update try.py

--- dialogflow-python-client-master/examples/slotfilling_example/try.py
+++ dialogflow-python-client-master/examples/slotfilling_example/try.py
@@ -1,6 +1,6 @@
-import apiai
+import  apiai
 import json
-
+# 'sessionId': 'b4146bdd67464cd098529af976f3a3d9'
 ai = apiai.ApiAI('cd4fbf5bf0bb489784266935eb751421')
 while True:
     print(u"> ", end=u"")
@@ -12,7 +12,7 @@ while True:
     request.query = user_message
 
     response = json.loads(request.getresponse().read())
-
+    print(response)
     result = response['result']
     action = result['action']
     actionIncomplete = result.get('actionIncomplete', False)
@@ -48,12 +48,39 @@ while True:
             date = [response['result']['parameters']['date']]
             time = [response['result']['parameters']['time']]
             if(len(number)!=1 and len(date) != 1 and len(time) != 1):
+                print("Class Name: "+response['result']['metadata']['intentName'])
+                print("Number of persons: ")
                 for x in range(len(number)):
                     print(number[x])
+                print("The date: ")
                 for x in range(len(date)):
                     print(date[x])
+                print("Time: ")
                 for x in range(len(time)):
                     print(time[x])
+
         # class edit reservation
+        if (response['result']['metadata']['intentName'] == 'edit reservation'):
+            number = [response['result']['parameters']['number']]
+            date = [response['result']['parameters']['date']]
+            time = [response['result']['parameters']['time']]
+            id = response['result']['parameters']['id']
+            if(len(number)!=1 and len(date) != 1 and len(time) != 1 and id != ''):
+                print("Class Name: "+response['result']['metadata']['intentName'])
+                print("ID: "+id)
+                print("The Updated number of persons: ")
+                for x in range(len(number)):
+                    print(number[x])
+                print("The Updated date: ")
+                for x in range(len(date)):
+                    print(date[x])
+                print("The Updated time: ")
+                for x in range(len(time)):
+                    print(time[x])
+
         # class delete reservation
-        
+        if (response['result']['metadata']['intentName'] == 'delete reservation'):
+            id = response['result']['parameters']['id']
+            if(id != ''):
+                print("Class Name: "+response['result']['metadata']['intentName'])
+                print("ID: "+id)
