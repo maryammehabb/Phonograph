3e1ebc362eb7df095011e4ed59123768afc5e317
NaghamSherif <34625219+NaghamSherif@users.noreply.github.com>
2019-06-09 22:03:39 +0200

    Signup-in in androidAPP

---
+++ AndroidAPP/Signup-in/connection/app/src/main/java/com/example/android/connection/signUp.java
@@ -0,0 +1,74 @@
+package com.example.android.connection;
+
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class signUp extends AppCompatActivity {
+
+    private EditText etName;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etAddress;
+    private EditText etPhone;
+
+    private TextView tvName;
+    private TextView tvEmail;
+    private TextView tvPassword;
+    private TextView tvAddress;
+    private TextView tvPhone;
+
+    private Button btnSignUp;
+
+    DbHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etName = (EditText)findViewById(R.id.etName);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        etAddress = (EditText)findViewById(R.id.etAddress);
+        etPhone = (EditText)findViewById(R.id.etPhone);
+        tvName = (TextView)findViewById(R.id.tvName);
+        tvEmail = (TextView)findViewById(R.id.tvEmail);
+        tvPassword = (TextView)findViewById(R.id.tvPassword);
+        tvAddress = (TextView)findViewById(R.id.tvAddress);
+        tvPhone = (TextView)findViewById(R.id.tvPhone);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        dbHelper = new DbHelper(getApplicationContext());
+        //dbHelper.createDatabase();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+
+            }
+        });
+
+
+    }
+
+    public void signUp (){
+        User user = new User();
+        user.setMail(etEmail.getText().toString());
+        user.setAddress(etAddress.getText().toString());
+        user.setDiscriminator("Customer");
+        user.setName(etName.getText().toString());
+        user.setPhone(etPhone.getText().toString());
+        user.setPassword(etPassword.getText().toString());
+        dbHelper.insertUser(user);
+
+
+    }
+}
